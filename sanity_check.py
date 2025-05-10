dab719af7a1af09743e7795a070591211404ba98
Mai Anh Vu <maianhuel@gmail.com>
2025-05-10 14:08:42 -0500

    update

--- sanity_check.py
+++ sanity_check.py
@@ -1,6 +1,8 @@
 import openslide
 # wsi = openslide.OpenSlide("/project/hnguyen2/mvu9/datasets/TGCA-datasets/KICH/bb078404-fd12-4f27-9d23-0fc412e76a52/TCGA-UW-A7GX-11Z-00-DX1.57C4FD28-5463-40C5-9E87-484F3326B395.svs")
-wsi = openslide.OpenSlide("~/datasets/TGCA-datasets/KICH/9ae213d2-e717-470d-93dc-f4e6f46d5425/TCGA-KL-8335-01Z-00-DX1.3F291515-CD98-4D8F-A255-A10A69139242.svs") 
+# wsi = openslide.OpenSlide("~/datasets/TGCA-datasets/KICH/9ae213d2-e717-470d-93dc-f4e6f46d5425/TCGA-KL-8335-01Z-00-DX1.3F291515-CD98-4D8F-A255-A10A69139242.svs") 
+path = os.path.expanduser("~/datasets/TGCA-datasets/KICH/9ae213d2-e717-470d-93dc-f4e6f46d5425/TCGA-KL-8335-01Z-00-DX1.3F291515-CD98-4D8F-A255-A10A69139242.svs")
+wsi = openslide.OpenSlide(path) 
 print(wsi.level_dimensions)
 
 import pandas as pd
