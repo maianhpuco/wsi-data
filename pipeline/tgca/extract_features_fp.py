313d6f094647a84276561a683b35a22bf807c73d
Mai Anh Vu <maianhuel@gmail.com>
2025-05-17 12:13:53 -0500

    update

--- pipeline/tgca/extract_features_fp.py
+++ pipeline/tgca/extract_features_fp.py
@@ -18,7 +18,7 @@ import pandas as pd
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")) 
 sys.path.append(base_path)
 
-# print("current base", base_path)
+print("current base", base_path)
 from utils.file_utils import save_hdf5
 from datasets.dataset_h5 import Dataset_All_Bags, Whole_Slide_Bag_FP
 from models import get_encoder
