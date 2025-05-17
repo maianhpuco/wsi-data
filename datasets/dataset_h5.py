b415d754e4a975ad3ae04b834dbea95a91b7adf8
Mai Anh Vu <maianhuel@gmail.com>
2025-05-17 12:00:06 -0500

    update

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -1,51 +1,199 @@
-from __future__ import print_function, division
+# from __future__ import print_function, division
+# import pandas as pd
+# from PIL import Image
+# import h5py
+# from random import randrange
+
+# def eval_transforms(pretrained=False):
+# 	if pretrained:
+# 		mean = (0.485, 0.456, 0.406)
+# 		std = (0.229, 0.224, 0.225)
+
+# 	else:
+# 		mean = (0.5,0.5,0.5)
+# 		std = (0.5,0.5,0.5)
+
+# 	trnsfrms_val = transforms.Compose(
+# 					[
+# 					 transforms.ToTensor(),
+# 					 transforms.Normalize(mean = mean, std = std)
+# 					]
+# 				)
+
+# 	return trnsfrms_val
+
+# class Whole_Slide_Bag(Dataset):
+# 	def __init__(self,
+# 		file_path,
+# 		pretrained=False,
+# 		custom_transforms=None,
+# 		target_patch_size=-1,
+# 		):
+# 		"""
+# 		Args:
+# 			file_path (string): Path to the .h5 file containing patched data.
+# 			pretrained (bool): Use ImageNet transforms
+# 			custom_transforms (callable, optional): Optional transform to be applied on a sample
+# 		"""
+# 		self.pretrained=pretrained
+# 		if target_patch_size > 0:
+# 			self.target_patch_size = (target_patch_size, target_patch_size)
+# 		else:
+# 			self.target_patch_size = None
+
+# 		if not custom_transforms:
+# 			self.roi_transforms = eval_transforms(pretrained=pretrained)
+# 		else:
+# 			self.roi_transforms = custom_transforms
+
+# 		self.file_path = file_path
+
+# 		with h5py.File(self.file_path, "r") as f:
+# 			dset = f['imgs']
+# 			self.length = len(dset)
+
+# 		self.summary()
+			
+# 	def __len__(self):
+# 		return self.length
+
+# 	def summary(self):
+# 		hdf5_file = h5py.File(self.file_path, "r")
+# 		dset = hdf5_file['imgs']
+# 		for name, value in dset.attrs.items():
+# 			print(name, value)
+
+# 		print('pretrained:', self.pretrained)
+# 		print('transformations:', self.roi_transforms)
+# 		if self.target_patch_size is not None:
+# 			print('target_size: ', self.target_patch_size)
+
+# 	def __getitem__(self, idx):
+# 		with h5py.File(self.file_path,'r') as hdf5_file:
+# 			img = hdf5_file['imgs'][idx]
+# 			coord = hdf5_file['coords'][idx]
+		
+# 		img = Image.fromarray(img)
+# 		if self.target_patch_size is not None:
+# 			img = img.resize(self.target_patch_size)
+# 		img = self.roi_transforms(img).unsqueeze(0)
+# 		return img, coord
+
+# class Whole_Slide_Bag_FP(Dataset):
+# 	def __init__(self,
+# 		file_path,
+# 		wsi,
+# 		pretrained=False,
+# 		custom_transforms=None,
+# 		custom_downsample=1,
+# 		target_patch_size=-1, 
+# 		patch_level=None, 
+# 		patch_size=None 
+#  		):
+# 		"""
+# 		Args:
+# 			file_path (string): Path to the .h5 file containing patched data.
+# 			pretrained (bool): Use ImageNet transforms
+# 			custom_transforms (callable, optional): Optional transform to be applied on a sample
+# 			custom_downsample (int): Custom defined downscale factor (overruled by target_patch_size)
+# 			target_patch_size (int): Custom defined image size before embedding
+# 		"""
+# 		self.pretrained=pretrained
+# 		self.wsi = wsi
+# 		if not custom_transforms:
+# 			self.roi_transforms = eval_transforms(pretrained=pretrained)
+# 		else:
+# 			self.roi_transforms = custom_transforms
+
+# 		self.file_path = file_path
+
+# 		with h5py.File(self.file_path, "r") as f:
+# 			dset = f['coords']
+   
+
+# 			# self.patch_level = f['coords'].attrs['patch_level']
+# 			# self.patch_size = f['coords'].attrs['patch_size']
+   
+# 			#======== MAIANH: EDIT THE ORIGINAL CODE HERE =====
+			 
+#     		# Try to get from HDF5 first; fallback to argument
+# 			self.patch_level = f['coords'].attrs.get('patch_level') if 'patch_level' in f['coords'].attrs else patch_level
+# 			self.patch_size = f['coords'].attrs.get('patch_size') if 'patch_size' in f['coords'].attrs else patch_size
+
+# 			# Only raise error if both sources failed
+# 			if self.patch_level is None:
+# 				print(f"⚠️ 'patch_level' not found in HDF5 or arguments for file: {self.file_path}")
+# 			if self.patch_size is None:
+# 				print(f"⚠️ 'patch_size' not found in HDF5 or arguments for file: {self.file_path}")
+		
+   
+# 			#=======END: EDIT ========== 
+# 			self.length = len(dset)
+# 			if target_patch_size > 0:
+# 				self.target_patch_size = (target_patch_size, ) * 2
+# 			elif custom_downsample > 1:
+# 				self.target_patch_size = (self.patch_size // custom_downsample, ) * 2
+# 			else:
+# 				self.target_patch_size = None
+# 		self.summary()
+  
+			
+# 	def __len__(self):
+# 		return self.length
+
+# 	def summary(self):
+# 		hdf5_file = h5py.File(self.file_path, "r")
+# 		dset = hdf5_file['coords']
+# 		for name, value in dset.attrs.items():
+# 			print(name, value)
+
+# 		print('\nfeature extraction settings')
+# 		print('target patch size: ', self.target_patch_size)
+# 		print('pretrained: ', self.pretrained)
+# 		print('transformations: ', self.roi_transforms)
+
+# 	def __getitem__(self, idx):
+# 		with h5py.File(self.file_path,'r') as hdf5_file:
+# 			coord = hdf5_file['coords'][idx]
+# 		img = self.wsi.read_region(coord, self.patch_level, (self.patch_size, self.patch_size)).convert('RGB')
+
+# 		if self.target_patch_size is not None:
+# 			img = img.resize(self.target_patch_size)
+# 		img = self.roi_transforms(img).unsqueeze(0)
+# 		return img, coord
+
+# class Dataset_All_Bags(Dataset):
+
+# 	def __init__(self, csv_path):
+# 		self.df = pd.read_csv(csv_path)
+	
+# 	def __len__(self):
+# 		return len(self.df)
+
+# 	def __getitem__(self, idx):
+# 		return self.df['slide_id'][idx]
+
+
+
+import numpy as np
 import pandas as pd
+
+from torch.utils.data import Dataset
+from torchvision import transforms
+
 from PIL import Image
 import h5py
-from random import randrange
-
-def eval_transforms(pretrained=False):
-	if pretrained:
-		mean = (0.485, 0.456, 0.406)
-		std = (0.229, 0.224, 0.225)
-
-	else:
-		mean = (0.5,0.5,0.5)
-		std = (0.5,0.5,0.5)
-
-	trnsfrms_val = transforms.Compose(
-					[
-					 transforms.ToTensor(),
-					 transforms.Normalize(mean = mean, std = std)
-					]
-				)
-
-	return trnsfrms_val
 
 class Whole_Slide_Bag(Dataset):
 	def __init__(self,
 		file_path,
-		pretrained=False,
-		custom_transforms=None,
-		target_patch_size=-1,
-		):
+		img_transforms=None):
 		"""
 		Args:
 			file_path (string): Path to the .h5 file containing patched data.
-			pretrained (bool): Use ImageNet transforms
-			custom_transforms (callable, optional): Optional transform to be applied on a sample
+			roi_transforms (callable, optional): Optional transform to be applied on a sample
 		"""
-		self.pretrained=pretrained
-		if target_patch_size > 0:
-			self.target_patch_size = (target_patch_size, target_patch_size)
-		else:
-			self.target_patch_size = None
-
-		if not custom_transforms:
-			self.roi_transforms = eval_transforms(pretrained=pretrained)
-		else:
-			self.roi_transforms = custom_transforms
-
+		self.roi_transforms = img_transforms
 		self.file_path = file_path
 
 		with h5py.File(self.file_path, "r") as f:
@@ -58,15 +206,12 @@ class Whole_Slide_Bag(Dataset):
 		return self.length
 
 	def summary(self):
-		hdf5_file = h5py.File(self.file_path, "r")
-		dset = hdf5_file['imgs']
-		for name, value in dset.attrs.items():
-			print(name, value)
+		with h5py.File(self.file_path, "r") as hdf5_file:
+			dset = hdf5_file['imgs']
+			for name, value in dset.attrs.items():
+				print(name, value)
 
-		print('pretrained:', self.pretrained)
 		print('transformations:', self.roi_transforms)
-		if self.target_patch_size is not None:
-			print('target_size: ', self.target_patch_size)
 
 	def __getitem__(self, idx):
 		with h5py.File(self.file_path,'r') as hdf5_file:
@@ -74,39 +219,29 @@ class Whole_Slide_Bag(Dataset):
 			coord = hdf5_file['coords'][idx]
 		
 		img = Image.fromarray(img)
-		if self.target_patch_size is not None:
-			img = img.resize(self.target_patch_size)
-		img = self.roi_transforms(img).unsqueeze(0)
-		return img, coord
+		img = self.roi_transforms(img)
+		return {'img': img, 'coord': coord}
 
 class Whole_Slide_Bag_FP(Dataset):
 	def __init__(self,
 		file_path,
 		wsi,
-		pretrained=False,
-		custom_transforms=None,
-		custom_downsample=1,
-		target_patch_size=-1, 
+		img_transforms=None, 
 		patch_level=None, 
-		patch_size=None 
- 		):
+		patch_size=None
+  	):
 		"""
 		Args:
 			file_path (string): Path to the .h5 file containing patched data.
-			pretrained (bool): Use ImageNet transforms
-			custom_transforms (callable, optional): Optional transform to be applied on a sample
-			custom_downsample (int): Custom defined downscale factor (overruled by target_patch_size)
-			target_patch_size (int): Custom defined image size before embedding
+			img_transforms (callable, optional): Optional transform to be applied on a sample
 		"""
-		self.pretrained=pretrained
 		self.wsi = wsi
-		if not custom_transforms:
-			self.roi_transforms = eval_transforms(pretrained=pretrained)
-		else:
-			self.roi_transforms = custom_transforms
+		self.roi_transforms = img_transforms
 
 		self.file_path = file_path
 
+		# with h5py.File(self.file_path, "r") as f:
+		# 	dset = f['coords']
 		with h5py.File(self.file_path, "r") as f:
 			dset = f['coords']
    
@@ -127,16 +262,12 @@ class Whole_Slide_Bag_FP(Dataset):
 				print(f"⚠️ 'patch_size' not found in HDF5 or arguments for file: {self.file_path}")
 		
    
-			#=======END: EDIT ========== 
+			#=======END: EDIT ==========  
+			# self.patch_level = f['coords'].attrs['patch_level']
+			# self.patch_size = f['coords'].attrs['patch_size']
 			self.length = len(dset)
-			if target_patch_size > 0:
-				self.target_patch_size = (target_patch_size, ) * 2
-			elif custom_downsample > 1:
-				self.target_patch_size = (self.patch_size // custom_downsample, ) * 2
-			else:
-				self.target_patch_size = None
+			
 		self.summary()
-  
 			
 	def __len__(self):
 		return self.length
@@ -148,8 +279,6 @@ class Whole_Slide_Bag_FP(Dataset):
 			print(name, value)
 
 		print('\nfeature extraction settings')
-		print('target patch size: ', self.target_patch_size)
-		print('pretrained: ', self.pretrained)
 		print('transformations: ', self.roi_transforms)
 
 	def __getitem__(self, idx):
@@ -157,10 +286,8 @@ class Whole_Slide_Bag_FP(Dataset):
 			coord = hdf5_file['coords'][idx]
 		img = self.wsi.read_region(coord, self.patch_level, (self.patch_size, self.patch_size)).convert('RGB')
 
-		if self.target_patch_size is not None:
-			img = img.resize(self.target_patch_size)
-		img = self.roi_transforms(img).unsqueeze(0)
-		return img, coord
+		img = self.roi_transforms(img)
+		return {'img': img, 'coord': coord}
 
 class Dataset_All_Bags(Dataset):
 
