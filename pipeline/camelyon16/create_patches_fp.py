d974f6e567ba075ac168f7aba8962e75a5def660
Mai Anh Vu <maianhuel@gmail.com>
2025-05-17 10:39:37 -0500

    update

--- pipeline/camelyon16/create_patches_fp.py
+++ pipeline/camelyon16/create_patches_fp.py
@@ -11,7 +11,7 @@ import sys
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 sys.path.append(base_path)
 print("Search path:", base_path)
-
+sys.path.append("src/externals/CLAM")
 # Internal imports
 from wsi_core.WholeSlideImage import WholeSlideImage
 from wsi_core.wsi_utils import StitchCoords
