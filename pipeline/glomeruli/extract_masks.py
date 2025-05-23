bdd66d0534bea2e94de0b63093564e4d75383925
Mai Anh Vu <maianhuel@gmail.com>
2025-05-23 16:06:49 -0500

    update

--- pipeline/glomeruli/extract_masks.py
+++ pipeline/glomeruli/extract_masks.py
@@ -57,7 +57,7 @@ def extract_annotations(db_path, image_dir, annotation_dir, preview=False):
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         
         print(cursor.fetchall())
-        cursor.execute("SELECT image_md5, geometry FROM RAW_ANNOTATIONS WHERE geometry IS NOT NULL;")
+        cursor.execute("SELECT image_md5, geometry FROM RAW_ANNOTATION WHERE geometry IS NOT NULL;")
         annotations = cursor.fetchall()
     except sqlite3.Error as e:
         print(f"  → [ERROR] Database error: {e}")
