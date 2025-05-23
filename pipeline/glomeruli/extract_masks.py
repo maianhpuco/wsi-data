4a6c02820d9c0a2c0021ccc4d1ffba5eaebd704a
Mai Anh Vu <maianhuel@gmail.com>
2025-05-23 16:05:45 -0500

    update

--- pipeline/glomeruli/extract_masks.py
+++ pipeline/glomeruli/extract_masks.py
@@ -55,9 +55,11 @@ def extract_annotations(db_path, image_dir, annotation_dir, preview=False):
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        print(cursor.fetchall())
-        cursor.execute("SELECT image_md5, geometry FROM annotation")
+        print("table name", cursor.fetchall())
+        cursor.execute("SELECT * FROM RAW_ANNOTATION")
         annotations = cursor.fetchall()
+        print("annotations", annotations)
+        
     except sqlite3.Error as e:
         print(f"  → [ERROR] Database error: {e}")
         return
