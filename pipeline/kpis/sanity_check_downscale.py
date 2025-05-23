ade34a3b392f0869ae9709656cb136a662bc4394
Mai Anh Vu <maianhuel@gmail.com>
2025-05-23 14:39:48 -0500

    update

--- pipeline/kpis/sanity_check_downscale.py
+++ pipeline/kpis/sanity_check_downscale.py
@@ -26,7 +26,7 @@ def main(args):
 
     for split in ["train", "test", "validation"]:
         pattern = config["patterns"]["slide_dir"][split]
-        save_root_base = config["sanity_downscale_dir"][split]
+        save_root_base = config["paths"]["sanity_downscale_dir"][split]
         slide_root = config["paths"]["slide_dir"][split]
 
         all_paths = glob.glob(pattern)
@@ -41,11 +41,12 @@ def main(args):
                 img_downscaled = downscale_image(img, downscale_factor)
 
                 relative_path = Path(img_path).relative_to(slide_root)
-                save_path = Path(save_root_base).parent / Path(split) / Path("Task2_WSI_level") / class_name / relative_path.name
+                save_path = Path(save_root_base) / class_name / relative_path.name
                 save_path.parent.mkdir(parents=True, exist_ok=True)
 
                 tiff.imwrite(str(save_path), img_downscaled)
-                
+                print(f"Saved downscaled image to: {save_path}")
+ 
             except Exception as e:
                 print(f"Failed to process {img_path}: {e}")
 
