bc97041275a248eecb2dae4e95ca45fa9c6e2177
Mai Anh Vu <maianhuel@gmail.com>
2025-05-26 17:49:54 -0500

    update

--- pipeline_cls/tcga/pyramidal_processing.py
+++ pipeline_cls/tcga/pyramidal_processing.py
@@ -12,6 +12,7 @@ def convert_to_pyramidal(input_dir, output_dir):
     os.makedirs(output_dir, exist_ok=True)
     tif_files = [f for f in os.listdir(input_dir) if f.endswith(".tif") or f.endswith(".tiff")]
     print(len(tif_files), "TIFF files found in", input_dir)
+    
     for filename in tqdm(tif_files, desc=f"Converting {input_dir}"):
         input_path = os.path.join(input_dir, filename)
         output_path = os.path.join(output_dir, filename)
