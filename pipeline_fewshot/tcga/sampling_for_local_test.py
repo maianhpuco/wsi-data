11841f1d0923a10d48593d986c902ca4e3b99e27
Mai Anh Vu <maianhuel@gmail.com>
2025-06-09 23:27:26 -0500

    update

--- pipeline_fewshot/tcga/sampling_for_local_test.py
+++ pipeline_fewshot/tcga/sampling_for_local_test.py
@@ -41,8 +41,8 @@ def main(args, config):
         print("src: ", src_file)
         print("dest: ", dest_file)
         
-        # files = shutil.copytree(src_file, dest_file)
-         
+        files = shutil.copytree(src_file, dest_file)
+        
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
