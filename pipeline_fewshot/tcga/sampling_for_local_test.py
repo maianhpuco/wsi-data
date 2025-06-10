52b591e943520d770d384c500f2e114b416f9e77
Mai Anh Vu <maianhuel@gmail.com>
2025-06-09 23:15:59 -0500

    update

---
+++ pipeline_fewshot/tcga/sampling_for_local_test.py
@@ -0,0 +1,54 @@
+import os
+import argparse
+import yaml
+from tqdm import tqdm
+import h5py
+import openslide
+from PIL import Image
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+import glob 
+import random 
+import shutil 
+
+def load_config(path):
+    with open(path, 'r') as f:
+        return yaml.safe_load(f)
+
+ 
+def main(args, config): 
+    
+    dest_parent_folder = '/project/hnguyen2/mvu9/sample_data' 
+    NUM_FILES = 5 
+    # data_names = ['kich', 'kirp', 'kirc', 'luad', 'luad']
+    # for name in data_names: 
+    dataset_name = config['dataset_name']
+    print(dataset_name)
+    file_paths = glob(f"/project/hnguyen2/mvu9/processing_datasets/processing_tcga_256/{dataset_name}/png_patches/patch_256x256_10x/patch_256x256_10x")
+    num_file = len(file_paths)
+    sample_indexes = random.randint(0, NUM_FILES)
+    dest_folder = os.path.join(dest_parent_folder, config['dataset_name']) 
+    
+    os.mkdir(os.path.join(dest_folder))
+    
+    for idx in sample_indexes: 
+        src_file = file_paths[idx] 
+        basename = os.path.basename(src_file) 
+        dest_file = os.path.join(dest_folder, basename)
+        print("src: ", src_file)
+        print("dest: ", dest_file)
+        
+        files = shutil.copytree(src_file, dest_file)
+         
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True)
+    parser.add_argument('--patch_size', type=int, required=True)
+    parser.add_argument('--magnification', type=str, required=True)
+
+    args = parser.parse_args()
+    config = load_config(args.config)
+    main(args, config)
+ 
+
